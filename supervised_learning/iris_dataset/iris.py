38790deb01a67f474964dd470d3dd8fb254fdfa9
Srinidhi Krishna <30746998+kss682@users.noreply.github.com>
2018-07-17 16:47:28 +0530

    using MLPClassifier

--- supervised_learning/iris_dataset/iris.py
+++ supervised_learning/iris_dataset/iris.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import numpy as np
-from sklearn import datasets,model_selection,preprocessing,svm
 
+import numpy as np
+from sklearn import datasets,model_selection,preprocessing
+from sklearn.neural_network import MLPClassifier
 
 #data processing
 iris=datasets.load_iris()
@@ -9,13 +9,13 @@ X=iris.data
 y=iris.target
 X=np.array(X)
 y=np.array(y)
-X=preprocessing.scale(X)
+X=preprocessing.MinMaxScaler().fit_transform(X)
 
 X_train,X_test,y_train,y_test=model_selection.train_test_split(X,y,test_size=0.2)
 #classifier
-for kernel in ['linear','rbf','poly']:
+for activation in ['relu','logistic','tanh']:
 
-	clf=svm.SVC(kernel=kernel,random_state=0,gamma=10,C=1)
+	clf=MLPClassifier(activation=activation,solver='lbfgs',random_state=1)
 	clf.fit(X_train,y_train)
 	print(clf.score(X_test,y_test))
 
