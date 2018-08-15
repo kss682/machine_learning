09992be94ab08e04c346fcece7c961db8f6b7be0
srinidhi <kss682@gmail.com>
2018-08-15 18:53:27 +0530

    pipeline

--- supervised_learning/iris_dataset/iris_svm.py
+++ supervised_learning/iris_dataset/iris_svm.py
@@ -1,7 +1,9 @@
 
 import numpy as np
-from sklearn import datasets,model_selection,preprocessing
+from sklearn import datasets,preprocessing
 from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
 
 #data processing
 iris=datasets.load_iris()
@@ -9,14 +11,14 @@ X=iris.data
 y=iris.target
 X=np.array(X)
 y=np.array(y)
-X=preprocessing.MinMaxScaler().fit_transform(X)
 
-X_train,X_test,y_train,y_test=model_selection.train_test_split(X,y,test_size=0.2)
+
+#X_train,X_test,y_train,y_test=model_selection.train_test_split(X,y,test_size=0.2)
 #classifier
 for kernel in ['linear','sigmoid','rbf']:
 
-        clf=svm.SVC(kernel=kernel,gamma=0.1,C=1)
-        clf.fit(X_train,y_train)
-        print(clf.score(X_test,y_test))
+        clf=make_pipeline(preprocessing.MinMaxScaler(),svm.SVC(kernel=kernel,gamma=0.1,C=1))
+        
+        print(cross_val_score(clf,X,y,cv=5).mean())
 
 
